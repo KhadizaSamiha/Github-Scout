b7c21ed2e207870fbff05151d237dbc783b5c574
Khadiza Samiha <khadizasamiha7@gmail.com>
2025-05-09 19:42:04 +0600

    correction

--- src/contexts/ThemeContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export const ThemeContext = createContext<{
-  darkMode: boolean;
-  toggleTheme: () => void;
-}>({
-  darkMode: false,
-  toggleTheme: () => {},
-});
